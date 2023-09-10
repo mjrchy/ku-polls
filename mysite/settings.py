5aea1428fc8bb0f2ee4d9ab03a6a6197e6cd7de8
Jiratchaya Thongsuthum <92805190+mjrchy@users.noreply.github.com>
2023-09-10 21:33:56 +0700

    Add allowed_hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,8 +26,7 @@ SECRET_KEY = config("SECRET_KEY", default='django-insecure-3q&13_!+q9u3s+8!ob$e@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool, default=True)
                
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv(), default="localhost,127.0.0.1")
 
 # Application definition
 
