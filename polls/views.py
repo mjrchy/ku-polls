0f43d69b9af8880b82920995e45783f01210156b
Jiratchaya Thongsuthum <92805190+mjrchy@users.noreply.github.com>
2023-09-10 17:30:42 +0700

    Improve get_queryset method and Add get method in DetailView

--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,7 @@ from django.views import generic
 
 from .models import Choice, Question
 from django.utils import timezone
+from django.contrib import messages
 
 
 class IndexView(generic.ListView):
@@ -13,12 +14,11 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """
-        Return the last five published questions (not including those set to be
-        published in the future).
+        Return all published questions (not including those set to be
+        published in the future) and exclude questions which has end date in the past
         """
-        return Question.objects.filter(
-            pub_date__lte=timezone.now()
-        ).order_by('-pub_date')[:5]
+        question_list = [question for question in Question.objects.all() if question.is_published() and question.can_vote()]
+        return question_list
 
 
 class DetailView(generic.DetailView):
@@ -29,7 +29,14 @@ class DetailView(generic.DetailView):
         """
         Excludes any questions that aren't published yet.
         """
-        return Question.objects.filter(pub_date__lte=timezone.now())
+        return Question.objects.filter(pub_date__lte=timezone.now(), end_date__gte=timezone.now())
+    
+    def get(self, request, pk):
+        selected_question = get_object_or_404(Question, pk=pk)
+        if not selected_question.can_vote():
+            messages.error(request, 'Voting is not allowed.')
+            return HttpResponseRedirect(reverse('polls:index')) 
+        return render(request, 'polls/detail.html', {'question': selected_question})
 
 
 class ResultsView(generic.DetailView):
@@ -54,3 +61,5 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+    
+
