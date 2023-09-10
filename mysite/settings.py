91c47cbc9841c3865146b286020932e3822f5ec7
Jiratchaya Thongsuthum <92805190+mjrchy@users.noreply.github.com>
2023-09-10 17:55:59 +0700

    Add python-decouple to the project

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,11 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3q&13_!+q9u3s+8!ob$e@ft75j_ws3g!3&5n!*2yp9o)-wj)^r'
+SECRET_KEY = config("SECRET_KEY", default='django-insecure-3q&13_!+q9u3s+8!ob$e@ft75j_ws3g!3&5n!*2yp9o)-wj)^r')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = config("DEBUG", cast=bool, default=True)
+               
 ALLOWED_HOSTS = []
 
 
