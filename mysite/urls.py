78ee8bc87da3acd8c5eeb7ed50c6699dc633c1b6
Jiratchaya Thongsuthum <92805190+mjrchy@users.noreply.github.com>
2023-09-12 14:58:26 +0700

    Add user authentication and login page

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,7 +18,8 @@ from django.urls import include, path
 from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')), 
     path('', RedirectView.as_view(url='/polls/')),
     path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls),
 ]
