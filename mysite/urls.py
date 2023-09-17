fac1522fd2bd2c87c9f81870982100853c90db1c
Jiratchaya Thongsuthum <92805190+mjrchy@users.noreply.github.com>
2023-09-17 16:57:31 +0700

    Add sign up url

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')), 
     path('', RedirectView.as_view(url='/polls/')),
     path('polls/', include('polls.urls')),
+    path('accounts/', include('django.contrib.auth.urls')), 
+    path('signup/', views.signup, name='signup'),
 ]
